e85804a7ce73e40adc8d4735a3dab53386a7a845
lemonzeng <yumengzeng.zeng@gmail.com>
2024-05-23 00:45:10 +0800

    Add GitHub link and full name to Labs, add Kanbas link to TOC

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,7 +8,6 @@ export default function TOC() {
                                 <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
                                 <li><a id="wd-k" href="#/Kanbas">Kanbas</a></li>
                         </ul>
-                        <h2>Table of Contents</h2>
                 </div>
         );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,8 +1,10 @@
+import React from "react";
 import Lab1 from "./Lab1";
 import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+
 export default function Labs() {
   return (
     <div>
@@ -14,6 +16,8 @@ export default function Labs() {
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
       </Routes>
+      <a id="wd-github" href="https://github.com/lemonzeng/kanbas-react-web-app.git">My GitHub Repository</a>
+      <p>Full Name: Yumeng Zeng</p>
     </div>
   );
 }
