37029728ea11269d6938da71b5b563d818ffb35c
lemonzeng <yumengzeng.zeng@gmail.com>
2024-05-24 18:02:32 +0800

    Modify the location of Github

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,7 +7,9 @@ export default function TOC() {
                                 <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
                                 <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
                                 <li><a id="wd-k" href="#/Kanbas">Kanbas</a></li>
+                                <li> <a id="wd-github" href="https://github.com/lemonzeng/kanbas-react-web-app.git">GitHub</a></li>
                         </ul>
+                        <p>Full Name: Yumeng Zeng</p>
                 </div>
         );
 }
