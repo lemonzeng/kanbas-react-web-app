513c7e651ce0f6990a7935e92913bfbd5239788d
lemonzeng <yumengzeng.zeng@gmail.com>
2024-05-28 12:27:33 +0800

    Finish thelab concerning  third week lecture

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,7 +1,49 @@
-export default function Lab2(){
+import "./index.css";
+import StyleAttribute from "./StyleAttribute";
+import IdSelector from "./IdSelector";
+import ClassSelectors from "./ClassSelector";
+import DocumentStructureStyle from "./DocumentStructureStyle";
+import ForegroundColor from "./ForegroundColor";
+import BackgroundColor from "./BackgroundColor";
+import Borders from "./Borders";
+import Paddings from "./Paddings";
+import Margins from "./Margins";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import RelativePosition from "./RelativePosition";
+import AbsolutePosition from "./AbsolutePosition";
+import FixedPosition from "./FixedPosition";
+import ZIndex from "./ZIndex";
+import FloatImages from "./FloatImages";
+import GridLayouot from "./GridLayout";
+import Flex from "./Flex";
+import ReactIconsSampler from "./ReactIconsSampler";
+import GridSystem from "./GridSystem";
+export default function Lab2() {
         return (
-         <div id="wd-lab2">
-                 <h2>Lab 2</h2>
-         </div>
+          <div id="wd-lab2" className="container">
+            <h2>Lab 2 - Cascading Style Sheets</h2>
+            <StyleAttribute />
+            <IdSelector />
+            <ClassSelectors />
+            <DocumentStructureStyle />
+            <ForegroundColor />
+            <BackgroundColor />
+            <Borders />
+            <Paddings />
+            <Margins />
+            <Corners />
+            <Dimensions />
+            <RelativePosition />
+            <AbsolutePosition /> 
+            <FixedPosition />
+            <ZIndex />
+            <FloatImages />
+            <GridLayouot />
+            <Flex />
+            <ReactIconsSampler />
+            <GridLayouot />
+          </div>
         );
- }
+}
+      
