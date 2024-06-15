90773955cb2c1be2afb476d3b648e0229eecce2a
lemonzeng <yumengzeng.zeng@gmail.com>
2024-06-15 18:19:53 +0800

    finish lab3

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -5,28 +5,39 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor"
 import Grades from './Grades'; 
 import { Navigate, Route, Routes } from "react-router";
+import { useParams } from 'react-router-dom';
 import { FaAlignJustify } from "react-icons/fa";
+import { courses } from "../Database";
 export default function Courses() {
-        return (
-                <div id="wd-courses">
-                  <h2 className="text-danger"><FaAlignJustify className="me-4 fs-4 mb-1" />Course 1234</h2>
-                  <hr />
-                  <div className="d-flex">
-                    <div className="d-none d-md-block ">
-                      <CoursesNavigation />
-                    </div>
-                    <div className="flex-fill">
-                      <Routes>
-                        <Route path="/" element={<Navigate to="Home" />} />
-                        <Route path="Home" element={<Home />} />
-                        <Route path="Modules" element={<Modules />} />
-                        <Route path="Assignments" element={<Assignments />} />
-                        <Route path="Assignments/:id" element={<AssignmentEditor />} />
-                        <Route path="Grades" element={<Grades />} />
-                      </Routes>
-                    </div>
-                  </div>
-                </div>
-              );
+  const { cid } = useParams();
+  console.log(cid);
+  const course = courses.find((course) => course._id === cid);
+  console.log(courses);
+  console.log(course);
+
+  return (
+    <div id="wd-courses">
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        {course && course.name}
+      </h2>
+      <hr />
+      <div className="d-flex">
+        <div className="d-none d-md-block ">
+          <CoursesNavigation />
+        </div>
+        <div className="flex-fill">
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route path="Assignments/:id" element={<AssignmentEditor />} />
+            <Route path="Grades" element={<Grades />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+        );
 }
         
--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -5,47 +5,39 @@ import { FaInbox } from "react-icons/fa6";
 import{ LiaBookSolid  } from "react-icons/lia";
 import {IoCalendarOutline, IoMailOutline} from "react-icons/io5";
 import { LiaCogSolid } from "react-icons/lia";
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function KanbasNavigation() {
-  const getNavLinkClass = ({ isActive }: { isActive: boolean }) => 
-    isActive ? "list-group-item text-center border-0 bg-white text-danger" : "list-group-item text-center border-0 bg-black text-white";
-  
+  const { pathname } = useLocation();
+  const links = [
+    { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
+    { label: "Courses",   path: "/Kanbas/Courses", icon: LiaBookSolid },
+    { label: "Calendar",  path: "/Kanbas/Calendar",  icon: IoCalendarOutline },
+    { label: "Inbox",     path: "/Kanbas/Inbox",     icon: FaInbox },
+    { label: "Labs",      path: "/Labs",             icon: LiaCogSolid },
+  ];
+
   return (
     <div id="wd-courses-navigation" className="list-group rounded-0" style={{ backgroundColor: "black", height: "100vh" }}>
-     <a id="wd-neu-link" target="_blank" 
+      <a id="wd-neu-link" target="_blank" 
         href="https://www.northeastern.edu/"
         className="list-group-item border-0 bg-black">
         <img src="/images/NEU.png" width="75px" />
       </a>
-      <NavLink id="wd-account-link" to="Account"
-        className="list-group-item text-center border-0 bg-black text-white">
-        <FaRegCircleUser className="fs-1 text-white" /><br />
+      <Link key="/Kanbas/Account" to="/Kanbas/Account" className={`list-group-item text-center border-0 bg-black
+            ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <FaRegCircleUser className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
+        <br />
         Account
-      </NavLink>
-      <NavLink id="wd-dashboard-link" to="Dashboard"
-        className={getNavLinkClass}>
-        <AiOutlineDashboard className="fs-1 text-danger"/><br />
-        Dashboard
-      </NavLink>
-      <NavLink id="wd-course-link" to="Courses" 
-        className={getNavLinkClass}>
-        <LiaBookSolid className="fs-1 text-danger" /><br />
-        Courses
-      </NavLink>
-      <NavLink id="wd-cNavLinklendar-link" to="Calendar" className={getNavLinkClass}>
-        <IoCalendarOutline className="fs-1 text-danger"/> <br />
-        Calendar
-      </NavLink>
-      <NavLink id="wd-inbox-link" to="Inbox" className={getNavLinkClass}>
-        <FaInbox className="fs-1 text-danger" /> <br />
-        Inbox
-      </NavLink>
-      <NavLink id="wd-labs-link" to="../Labs" className={getNavLinkClass}>
-        <LiaCogSolid className="fs-1 text-danger" /> <br />
-        Labs
-      </NavLink>
+      </Link>
+      {links.map((link) => (
+        <Link key={link.path} to={link.path} className={`list-group-item bg-black text-center border-0
+              ${pathname.includes(link.label) ? "text-danger bg-white" : "text-white bg-black"}`}>
+          {link.icon({ className: "fs-1 text-danger"})}
+          <br />
+          {link.label}
+        </Link>
+      ))}
     </div>
-
   );
 }
