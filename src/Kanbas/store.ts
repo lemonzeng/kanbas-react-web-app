bc8bc0b951190f77cbd7bf65743eaa84c186ae1a
lemonzeng <yumengzeng.zeng@gmail.com>
2024-06-30 19:12:37 +0800

    modify the name of reducer

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
-import assignmentsRuducer from "./Courses/Assignments/reducer";
+import assignmentsReducer from "./Courses/Assignments/reducer";
 const store = configureStore({
   reducer: {
     modulesReducer,
